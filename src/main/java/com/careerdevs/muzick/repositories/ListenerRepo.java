582bb00de5c06a2a7cdaec578dee0e53ec694caf
Simon Morris <simonjmorris213@gmail.com>
2022-06-27 19:54:32 -0500

    commit

--- src/main/java/com/careerdevs/muzick/repositories/ListenerRepo.java
+++ src/main/java/com/careerdevs/muzick/repositories/ListenerRepo.java
@@ -2,8 +2,14 @@ package com.careerdevs.muzick.repositories;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.careerdevs.muzick.models.Listener;
 
 public interface ListenerRepo extends JpaRepository<Listener, Long> {
     List<Listener> findAllByAge(Integer age);
+
+    @Query("Select * FROM listener WHERE name LIKE '%?1%'")
+    List<Listener> findAllByPartialName(String query);
+
 }
