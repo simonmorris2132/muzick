b85b02368a21673d47b789bdd3a637ec9f2cad9b
Simon Morris <simonjmorris213@gmail.com>
2022-06-27 20:38:58 -0500

    commit

--- src/main/java/com/careerdevs/muzick/controllers/ListenerController.java
+++ src/main/java/com/careerdevs/muzick/controllers/ListenerController.java
@@ -1,5 +1,6 @@
 package com.careerdevs.muzick.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -8,15 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.careerdevs.muzick.models.Listener;
-import com.mysql.cj.protocol.x.Ok;
+import com.careerdevs.muzick.repositories.ListenerRepo;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/listeners")
 public class ListenerController {
     
+    @Autowired
+    private ListenerRepo repo;
+
     @GetMapping("/test")
     public ResponseEntity<String> rootRoute() {
        return new ResponseEntity<>("Okay", HttpStatus.OK);
@@ -24,7 +27,8 @@ public class ListenerController {
 
     @PostMapping("/")
     public ResponseEntity<?> createListener(@RequestBody Listener newListener) {
-        
+        Listener listener = repo.save(newListener);
+        return new ResponseEntity<>(listener, HttpStatus.CREATED);
     }
 
 }
