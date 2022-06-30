e14ad17c3fd103f97a0aff769a75c6bafb9fe2a3
Simon Morris <simonjmorris213@gmail.com>
2022-06-29 21:27:28 -0500

    commit

--- src/main/java/com/careerdevs/muzick/controllers/NoteController.java
+++ src/main/java/com/careerdevs/muzick/controllers/NoteController.java
@@ -1,7 +1,6 @@
 package com.careerdevs.muzick.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
 import com.careerdevs.muzick.models.Listener;
 import com.careerdevs.muzick.models.Note;
 import com.careerdevs.muzick.repositories.ListenerRepo;
