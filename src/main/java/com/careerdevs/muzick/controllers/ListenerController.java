3d481fc1d825d56e6c2afb813a5cd39b43b93b69
Simon Morris <simonjmorris213@gmail.com>
2022-06-29 21:26:24 -0500

    commit

--- src/main/java/com/careerdevs/muzick/controllers/ListenerController.java
+++ src/main/java/com/careerdevs/muzick/controllers/ListenerController.java
@@ -1,7 +1,6 @@
 package com.careerdevs.muzick.controllers;
 
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,10 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import com.careerdevs.muzick.models.Listener;
-import com.careerdevs.muzick.models.Note;
 import com.careerdevs.muzick.repositories.ListenerRepo;
 
 @CrossOrigin
