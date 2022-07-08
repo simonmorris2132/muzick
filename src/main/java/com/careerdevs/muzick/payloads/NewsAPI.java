31509f1a7aa2e5938638239ed5aa982dae11bf4a
Simon Morris <simonjmorris213@gmail.com>
2022-07-07 19:27:06 -0500

    commit

--- src/main/java/com/careerdevs/muzick/payloads/NewsAPI.java
+++ src/main/java/com/careerdevs/muzick/payloads/NewsAPI.java
@@ -1,5 +1,11 @@
 package com.careerdevs.muzick.payloads;
 
+import java.util.List;
+
 public class NewsAPI {
     
+    private String status;
+    private Integer totalResults;
+    private List<Article> articles;
+
 }
