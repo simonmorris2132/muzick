d45b6d6ffec0a5af407ff812a0568f9e97e38b06
Simon Morris <simonjmorris213@gmail.com>
2022-07-07 19:36:16 -0500

    commit

---
+++ src/main/java/com/careerdevs/muzick/controllers/NewsController.java
@@ -0,0 +1,12 @@
+package com.careerdevs.muzick.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/news")
+public class NewsController {
+    
+}
