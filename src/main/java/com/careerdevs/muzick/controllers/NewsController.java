87b02aa4a03e7dcb72aa8471cd8c0db793ac431c
Simon Morris <simonjmorris213@gmail.com>
2022-07-22 23:11:16 -0400

    commit

--- src/main/java/com/careerdevs/muzick/controllers/NewsController.java
+++ src/main/java/com/careerdevs/muzick/controllers/NewsController.java
@@ -2,7 +2,6 @@ package com.careerdevs.muzick.controllers;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.careerdevs.muzick.payloads.Article;
 import com.careerdevs.muzick.payloads.NewsAPI;
 
